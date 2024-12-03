3a3a09289ba4e79280ac3d45b4a58ebe91a992e4
boypiss <troymeggie@gmail.com>
2024-12-02 19:22:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Route for the homepage
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# Route for the "More Information" page
+@app.route("/info")
+def info():
+    return render_template("info.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route("/text")
+def text_page():
+    return render_template("text.html")
